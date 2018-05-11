67701de081ad2a741b2e5db63bbe0334e620ed08
jiangminyi1 <jmy514341490@vip.qq.com>
2018-05-11 08:57:12 +0800

    sdf

---
+++ Assets/Scriptes/TestScirpts/Coroutine/UTools.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace xxx.xx{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class UTools : MonoBehaviour 
+	{
+        private Transform FindChindForName(Transform tran,string objName) {
+            if (tran.Find(objName) == null && tran.childCount == 0) {
+                return null;
+            }
+            else if (tran.Find(objName) == null && tran.childCount != 0) {
+                for (int i = 0; i < tran.childCount; i++) {
+
+                }
+            }
+            else {
+                return transform.Find(objName);
+            }
+        }
+	}
+}
+
